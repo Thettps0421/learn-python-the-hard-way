e97033cd2bc0bb94932b00d5b6cb25951c7e2807
Kuzumi Taiga <kuzumi1190@gmail.com>
2020-08-10 13:36:36 +0700

    Add files via upload

---
+++ 36-2.py
@@ -0,0 +1,10 @@
+count = 0
+while (count < 9):
+    print("The count is:", count)
+    count = count + 1
+
+while (count > -9):
+    print("The count is: ", count)
+    count = count -1
+
+print("Good bye!")
